821db7ae2e383d566725652a4898560bc475bf94
Daniel <cermak222@gmail.com>
2019-08-03 20:52:46 -0500

    add js starter layout

---
+++ js/script.js
@@ -0,0 +1,24 @@
+/*----- constants -----*/ 
+const suit = []
+const rank = []
+
+let masterDeck
+
+/*----- app's state (variables) -----*/ 
+let scores, result, winner
+let shuffleDeck
+
+/*----- cached element references -----*/ 
+let score
+
+
+
+let result
+
+let shuffledContainer
+
+/*----- event listeners -----*/ 
+// button , click, render shuffle deck
+
+/*----- functions -----*/
+// render shuffle, render deck, build master deck
