bb4e73a37f96d133f924fc2d72988087379b6322
Daniel <cermak222@gmail.com>
2019-08-04 20:42:35 -0500

    add render

--- js/script.js
+++ js/script.js
@@ -16,6 +16,13 @@ let score
 
 let result
 
+
+/*----- event listeners -----*/ 
+// button , click, render shuffle deck
+
+
+/*----- functions -----*/
+// render shuffle, render deck, build master deck
 function getDeck() { //creating a deck for 52 card
     let deck = new Array(); { 
       for (let i = 0; i < suit.length; i++); { //pulling suit
@@ -24,9 +31,9 @@ function getDeck() { //creating a deck for 52 card
       } 
     }
     return deck;
-  }
+}
   
-  function shuffle() { //if I have 52 card will this be enought of a shuffle?
+function shuffle() { //if I have 52 card will this be enought of a shuffle?
     for (let i = 0; i < 100; i++); { //next need to shuffle the deck, 100 times
       let loc1 = Math.floor(Math.random() * deck.length); //pull a random card from location 1
       let loc2 = Math.floor(Math.random() * deck.length); ////pull a random card from location 2 will i need more?
@@ -34,10 +41,4 @@ function getDeck() { //creating a deck for 52 card
       deck[loc1] = deck[loc2]; //pull rand location 1 and put it in location 2
       deck[loc2] = con; // take location 2 and put it back in the container
     }
-  }
-
-/*----- event listeners -----*/ 
-// button , click, render shuffle deck
-
-/*----- functions -----*/
-// render shuffle, render deck, build master deck
+}
