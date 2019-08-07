e43e8fa7b5b15d86fa976b98891557dddfc9a643
Daniel <cermak222@gmail.com>
2019-08-07 08:11:53 -0500

    refactoring function

--- js/script.js
+++ js/script.js
@@ -1,54 +1,45 @@
 /*----- constants -----*/ 
-// let deck = new Array();
-var suits = ['s', 'c', 'd', 'h' ];
-var ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'k', 'A'];
+var suits = ['spades', 'clubs', 'diamonds', 'hearts' ];
+var ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'k', 'A'];
 
-var masterDeck = buildMasterDeck();
-// renderDeckInContainer(masterDeck, document.getElementById('master-deck-container'));
+var masterDeck; 
+
 /*----- app's state (variables) -----*/ 
 var score, result, winner;
-var shuffledDeck;
+var shuffledDeck, players, currentPlayer; 
+
 
 /*----- cached element references -----*/ 
-// const scoreEls = {
-//   p1: document.getElementById(p1-score),
-//   dr: document.getElementById(dr-score),
-//   p2: document.getElementById(p2-score),
-// };
-
-// const resultEls = {
-//   player1: 
-//    {
-//     gameEls: document.getElementById(p1-result),
-//     imgEl: document.querySelector('#p1-result img')
-//   },
-//   player2:
-//    {
-//     gameEls: document.getElementById(p2-result),
-//     imgEl: document.querySelector('#p2-result img')
-//     }
-// };
-
 var shuffledContainer = document.getElementById('shuffled-deck-container');
 
 /*----- event listeners -----*/ 
-// button , click, 
+ 
 document.getElementById("hit").addEventListener('click', hit);
 document.getElementById("stay").addEventListener('click', stay);
-document.getElementById('start').addEventListener('click', renderShuffledDeck);
+document.getElementById('start').addEventListener('click', start);
 /*----- functions -----*/
-// render, shuffle, new deck, players
-// init();
 
+init();
 
-function shuffledDeck() {
-  // create a copy of the masterDeck (leave masterDeck untouched!)
+function init() {
+  players = {
+    'player1': [],
+    'player2': []
+  };
+  // currentPlayer = 'player1'
+  masterDeck = buildMasterDeck()
+  shuffledDeck = shuffledDeck()
+  // dealCard()
+  // renderHand(playerHand)
+}
+
+function shuffledDeck() { // create a copy of the masterDeck (leave masterDeck untouched!)
   var tempDeck = masterDeck.slice();
   shuffledDeck = [];
   while (tempDeck.length) {
     var rndIdx = Math.floor(Math.random() * tempDeck.length);
     shuffledDeck.push(tempDeck.splice(rndIdx, 1)[0]);
-  }
+  } 
   // console.log(shuffledDeck)
   return shuffledDeck;
 
@@ -58,27 +49,35 @@ function buildMasterDeck() {
   var deck = [];
   suits.forEach(function(suit) {
     ranks.forEach(function(rank) {
-      deck.push({
-        // the 'face' property maps to the CSS classes for cards
-        face: `${suit}${rank}`,
-        // the 'value' property is set for blackjack, not war
-        value: Number(rank) || (rank === 'A' ? 11 : 10)
+      deck.push({ 
+        face: `${suit}-r${rank}`, // the 'face' property maps to the CSS classes for cards
+        value: Number(rank) || (rank === 'A' ? 11 : 10) // the 'value' property is set for blackjack, not war
       });
     });
   });
   return deck;
 }
-  
-function dealCard () {
-  for (i = 0; i < 2; i++){
-    for (var x = 0; x < players.length; x++ ) {
-      masterDeck.pop();
-      player1.hand.push(card);
-    }
+
+function dealCard(numCards) {
+  for (i = 0; i < numCards; i++) {
+    players[currentPlayer].push(shuffledDeck.pop());
   }
-
-  let playerHand = [];
-  playerHand.push(shuffledDeck.pop())
+  displayHand()
+  checkBust(countCards(players[currentPlayer]))
 };
-// renderCard(card, x);
+ 
+function displayHand() {
+  document.getElementById(currentPlayer).innerHTML = "" //clearing hand
 
+  var hand = players[currentPlayer]
+  for (var i = 0; i < hand.length; i++ ) {
+    let folder = hand[i].face.split("-")[0]
+    let file = hand[i].face
+    var cardImg = document.createElement("img");
+    cardImg.src = `./images/${folder}/${file}.svg`
+    document.getElementById(currentPlayer).append(cardImg)
+    // playerHandhtml.append(cardImg);
+  }
+  // document.getElementById(currentPlayer + "-score").innerHTML = "" // need this for when i show score
+  // document.getElementById(currentPlayer + "-score").append(countCards(players[currentPlayer]))
+}
